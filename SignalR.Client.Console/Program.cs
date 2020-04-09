ba30e2fd6f76a80e37500d9959dbaed30a5a7295
Fredrick Lutterodts <fredrick@hubtel.com>
2020-04-09 19:14:18 +0000

    minimalistic app to demo signalr implementation for server and client (console app)

--- SignalR.Client.Console/Program.cs
+++ SignalR.Client.Console/Program.cs
@@ -13,11 +13,10 @@ namespace SignalR.Client.Console
             System.Console.ReadKey();
 
             var connection = new HubConnectionBuilder()
-                //.WithUrl("http://localhost:5000/chathub")
-                .WithUrl("http://18.185.227.32:5000/chathub")
+                .WithUrl("http://localhost:5000/chathub")
                 .WithAutomaticReconnect()
                 .Build();
-            //connection.On<string>("NewOrder", message => { System.Console.WriteLine($"Message received from server: {message}"); });
+            connection.On<string>("NewOrder", message => { System.Console.WriteLine($"Message received from server: {message}"); });
             await connection.StartAsync();
             System.Console.WriteLine("Listening. Press a key to quit");
 
